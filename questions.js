c50ca322db02a7b8fb157fc01e8f4a6549f28e15
Harshavardhan Verma <88719299+Harshvardhan-Verma@users.noreply.github.com>
2024-01-17 22:11:57 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,56 @@
+let questions = [{
+        numb: 1,
+        question: "What is (PHI)?",
+        answer: "Protected Health Information",
+        options: [
+            "Public Hospitality Index",
+            "Protected Health Information",
+            "Protected Human Index",
+            "Pure Health Indicator "
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Where is Emitrr information located?",
+        answer: " US with Amazon Web Services (AWS)",
+        options: [
+            " US with Amazon Web Services (AWS)",
+            " Germany with ScalaHosting",
+            " US with ScalaHosting",
+            " Germany with Amazon Web Services (AWS)"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+];
